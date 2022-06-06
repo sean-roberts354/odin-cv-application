c5503881bbeba23107cb3970eeb55f38833ba268
Sean Roberts <sean.roberts354@gmail.com>
2022-06-06 17:15:00 -0400

    Create Work component and add needed inputs

---
+++ src/components/Work.js
@@ -0,0 +1,95 @@
+import React from "react";
+
+export default class Work extends React.Component {
+    render() {
+        const work = Object.values(this.props.data);
+        const area = "work";
+
+        return (
+            <fieldset>
+                <legend>Work Information</legend>
+                {work.map((item) => {
+                    return (
+                        <div key={`job${item.id}`} className="work-item">
+                            <label htmlFor="company">
+                                Company:
+                                <input
+                                    type="text"
+                                    id="company"
+                                    value={item.company}
+                                    onChange={(e) => {
+                                        this.props.handleInput(
+                                            e,
+                                            area,
+                                            item.id
+                                        );
+                                    }}
+                                />
+                            </label>
+                            <label htmlFor="title">
+                                Job Title:
+                                <input
+                                    type="text"
+                                    id="jobTitle"
+                                    value={item.jobTitle}
+                                    onChange={(e) => {
+                                        this.props.handleInput(
+                                            e,
+                                            area,
+                                            item.id
+                                        );
+                                    }}
+                                />
+                            </label>
+                            <label htmlFor="description">
+                                Job Description:
+                                <textarea
+                                    id="jobDescription"
+                                    value={item.jobDescription}
+                                    onChange={(e) => {
+                                        this.props.handleInput(
+                                            e,
+                                            area,
+                                            item.id
+                                        );
+                                    }}
+                                />
+                            </label>
+                            <label htmlFor="jobStartYear">
+                                Start Date:
+                                <input
+                                    type="date"
+                                    id="jobStartYear"
+                                    value={item.jobStartYear}
+                                    onChange={(e) => {
+                                        this.props.handleInput(
+                                            e,
+                                            area,
+                                            item.id
+                                        );
+                                    }}
+                                />
+                            </label>
+                            <label htmlFor="jobEndYear">
+                                End Date:
+                                <input
+                                    type="date"
+                                    id="jobEndYear"
+                                    value={item.jobEndYear}
+                                    onChange={(e) => {
+                                        this.props.handleInput(
+                                            e,
+                                            area,
+                                            item.id
+                                        );
+                                    }}
+                                />
+                            </label>
+                        </div>
+                    );
+                })}
+                
+            </fieldset>
+        );
+    }
+}
